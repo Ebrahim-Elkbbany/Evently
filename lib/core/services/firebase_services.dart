f3e3ec08f4b71b681f30dcb2aefdaf921e3c493f
Ebrahim-Elkbbany <136932497+Ebrahim-Elkbbany@users.noreply.github.com>
2026-04-19 08:09:21 +0200

    feat: add Google Sign-In integration and update dependencies

--- lib/core/services/firebase_services.dart
+++ lib/core/services/firebase_services.dart
@@ -4,6 +4,7 @@ import 'package:evently/features/login/data/models/sign_in_model.dart';
 import 'package:evently/features/sign_up/data/models/sign_up_model.dart';
 import 'package:evently/features/sign_up/data/models/user_model.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 abstract class FirebaseServices {
   static FirebaseFirestore firestore = FirebaseFirestore.instance;
@@ -79,6 +80,20 @@ abstract class FirebaseServices {
       imagePath: eventModel.imagePath,
     );
     return doc.set(modelWithId);
+  }
+
+  static Future<UserCredential?> signInWithGoogle() async {
+    await GoogleSignIn.instance.initialize();
+    final googleUser = await GoogleSignIn.instance.authenticate();
+
+    GoogleSignInAuthentication googleAuth = googleUser.authentication;
+    final credential = GoogleAuthProvider.credential(
+      accessToken: googleAuth.idToken,
+      idToken: googleAuth.idToken,
+    );
+
+    // 5. إتمام تسجيل الدخول
+    return await FirebaseAuth.instance.signInWithCredential(credential);
   }
 
   static Future<void> logout() async {
